ce3445eeac62d65a4b8907ef14772a0e068e41c5
OscarMarin-ITCG <79126324+OscarMarin-ITCG@users.noreply.github.com>
2021-03-04 05:39:42 -0600

    Add files via upload

---
+++ Agenda.java
@@ -0,0 +1,13 @@
+/* 
+   Programador: 
+   Oscar Fernando Marin Adame
+*/
+
+public class Agenda 
+{
+   public static void main(String args[])
+   {
+      JFrameIngreso Panel = new JFrameIngreso();
+      
+   }
+}
